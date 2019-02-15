4cc5fcb80d721d9d6f43726b195e693c0dfdc64f
David <david-d25@yandex.ru>
2019-02-15 23:15:52 +0300

    Initial

---
+++ JSONParseException.java
@@ -0,0 +1,5 @@
+public class JSONParseException extends Exception {
+    public JSONParseException(String message) {
+        super(message);
+    }
+}
---
+++ JSONType.java
@@ -0,0 +1,17 @@
+﻿public enum JSONType {
+    OBJECT("объект"),
+    ARRAY("массив"),
+    STRING("строка"),
+    NUMBER("число"),
+    BOOLEAN("логический");
+
+    String name;
+
+    JSONType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
